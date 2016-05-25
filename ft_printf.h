5fd5e4e4848fc6b725f3478deb8a8586364a0048
Kalilo <kalilohansmann@gmail.com>
2016-05-25 10:32:20 +0200

    added header file

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/25 10:26:04 by khansman          #+#    #+#             */
+/*   Updated: 2016/05/25 10:31:57 by khansman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+
+# include "libft.h"
+
+unsigned long int	ft_strlen_l(const long char *str);
+void				ft_put_hex(long unsigned int num);
+void				ft_put_oct(long unsigned int num);
+
+#endif
