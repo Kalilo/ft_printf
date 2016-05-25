fb4e164cf38c7a9705c191837b12d23793562c6a
Kalilo <kalilohansmann@gmail.com>
2016-05-25 10:19:09 +0200

    fixed norm error regarding spaces

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/23 07:54:06 by khansman          #+#    #+#             */
-/*   Updated: 2016/05/25 09:09:12 by ghavenga         ###   ########.fr       */
+/*   Updated: 2016/05/25 09:54:00 by khansman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,43 @@
 
 void 				ft_add_param(char type, void *param)
 {
-    if (type == 'S')
-        //ft_putstr_l((long char *)param);
-    else if (type == 'i')
-        ft_putnbr((int)param);
-    else if (type == 's')
-        ft_putstr((char *)param);
+	if (type == 'S')
+		//ft_putstr_l((long char *)param);
+	else if (type == 'i' || type == 'd')
+		ft_putnbr((int)param);
+	else if (type == 's')
+		ft_putstr((char *)param);
+	else if (type == 'o')
+		ft_put_oct((unsigned int)param);
 }
 
 int					ft_printf(const char *str, ...)
 {
-    va_list         format;
-    unsigned int    k;
-    int             insert;
+	va_list			format;
+	unsigned int	k;
+	int				insert;
 
-    k = 0;
-    insert = 0;
-    va_start(format, str);
-    while (str[k] != '\0')
-    {
-        if (str[k] == '%' && insert == 0)
-            insert = 1;
-        else if (insert == 1 && str[k] != '%')
-        {
-            ft_add_param(str[k], va_arg(format, void *));
-            insert = 0;
-        }
-        else if (insert == 1 && str[k] == '%')
-        {
-            ft_putchar('%');
-            insert = 0;
-        }
-        else
-            ft_putchar(str[k]);
-        k++;
-    }
-    va_end(format);
-    return (1);
+	k = 0;
+	insert = 0;
+	va_start(format, str);
+	while (str[k] != '\0')
+	{
+		if (str[k] == '%' && insert == 0)
+			insert = 1;
+		else if (insert == 1 && str[k] != '%')
+		{
+			ft_add_param(str[k], va_arg(format, void *));
+			insert = 0;
+		}
+		else if (insert == 1 && str[k] == '%')
+		{
+			ft_putchar('%');
+			insert = 0;
+		}
+		else
+			ft_putchar(str[k]);
+		k++;
+	}
+	va_end(format);
+	return (1);
 }
