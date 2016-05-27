b898acd181a53a75a5a28248df3e33bb748832c6
Kalilo <kalilohansmann@gmail.com>
2016-05-27 11:23:03 +0200

    Added ft_wchar_len

--- ft_printf.h
+++ ft_printf.h
@@ -54,5 +54,6 @@ size_t				ft_put_oct(long unsigned int num);
 size_t				ft_put_uint(unsigned int num);
 void				ft_put_float(long double num, unsigned int dec);
 size_t				ft_put_bin(const long long unsigned int num);
+int					ft_wchar_len(wchar_t *ws);
 
 #endif
